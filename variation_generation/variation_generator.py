d0fa6ed10f8d7f816f5b8de1f0efd1af6e4266dc
Matt Michaud <matthew.michaud@ramseysolutions.com>
2021-01-05 10:15:48 -0600

    update example

--- variation_generation/variation_generator.py
+++ variation_generation/variation_generator.py
@@ -67,4 +67,4 @@ def generate_variations(input_file, output_file=None, verbose=True):
 
 
 
-generate_variations(input_file='sample_input.txt')
+generate_variations(input_file='sample_input.txt', output_file='sample_output.txt')
